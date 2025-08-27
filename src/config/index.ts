af4d2ed1c6f69ebd58a79a753901b48016b824be
dhaniaffa <dhaniaffa@gmail.com>
2025-08-27 15:41:25 +0700

    add .env.example file and update dotenv configuration to load environment-specific files

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+const env = process.env.NODE_ENV || 'development';
+const envFile =
+  env === 'production' ? '.env.production' : env === 'development' ? '.env.development' : '.env';
+dotenv.config({ path: envFile });
 
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
