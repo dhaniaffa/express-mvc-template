bea055667adf77716aa47ecfdd4e284e82c06aa0
dhaniaffa <dhaniaffa@gmail.com>
2025-08-29 09:09:44 +0700

    Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes/index';
 import { PORT } from './config';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
