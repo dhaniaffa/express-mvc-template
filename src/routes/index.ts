9c41f7a389ab156b46e1e04744cf3a32d91b0677
dhaniaffa <dhaniaffa@gmail.com>
2025-11-01 11:06:39 +0700

    add welcome message endpoint to API root

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,12 @@ import userRoutes from './user.routes';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to the API',
+  });
+});
+
 router.use('/users', userRoutes);
 
 export default router;
